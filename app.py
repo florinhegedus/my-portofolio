ffaec5d5a018fae4b05d3e0c563c339601889773
florinhegedus <florinxhgds@gmail.com>
2024-09-14 13:01:02 +0300

    check CICD on GC

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World from Flask on Google Cloud!'
+    return 'Hello, World from Flask on Google Cloud! Hello from Florin'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
